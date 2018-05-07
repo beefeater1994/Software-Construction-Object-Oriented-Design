bf9e57a6215c9d79ebdcaa485c527631165a8e85
beefeater <ruihanf2016@gmail.com>
2018-05-07 00:36:55 -0400

    second commit

---
+++ long-form-problem-starters-master/CoffeeMaker/src/model/BeansAmountException.java
@@ -0,0 +1,14 @@
+package model;
+
+public class BeansAmountException extends Exception{
+    private double beans;
+
+    public BeansAmountException(double beans){
+
+    }
+
+    public double getBeans(){
+        return  0;
+    }
+
+}
